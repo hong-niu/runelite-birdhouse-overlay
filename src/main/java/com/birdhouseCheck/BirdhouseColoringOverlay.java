280143f85c2a45e0ef4858fec1fc93f075f8c911
Cody Li <codyli332@gmail.com>
2021-12-27 12:55:28 -0500

    Clean up and refactor logic, tidy up code style (#1)
    * Removing wildcard imports, deleting unused code in CheckPlugin
    
    * refactor render with birdhouseStateRenderer helper method
    
    * NPE check for local location, fixing bracket styling
    
    * fix nullpointers, fix location names
    
    Co-authored-by: hongniu99 <hongniu1024@gmail.com>

--- src/main/java/com/birdhouseCheck/BirdhouseColoringOverlay.java
+++ src/main/java/com/birdhouseCheck/BirdhouseColoringOverlay.java
@@ -6,120 +6,122 @@ import java.awt.Shape;
 import java.util.Objects;
 import javax.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
-import net.runelite.api.*;
+import net.runelite.api.Client;
+import net.runelite.api.GameObject;
+import net.runelite.api.Point;
 import net.runelite.api.coords.LocalPoint;
 import net.runelite.client.ui.overlay.Overlay;
 import net.runelite.client.ui.overlay.OverlayLayer;
 import net.runelite.client.ui.overlay.OverlayPosition;
 import net.runelite.client.ui.overlay.OverlayPriority;
 import net.runelite.client.ui.overlay.outline.ModelOutlineRenderer;
-import static net.runelite.api.VarPlayer.*;
 
+import static net.runelite.api.VarPlayer.BIRD_HOUSE_MEADOW_NORTH;
+import static net.runelite.api.VarPlayer.BIRD_HOUSE_MEADOW_SOUTH;
+import static net.runelite.api.VarPlayer.BIRD_HOUSE_VALLEY_NORTH;
+import static net.runelite.api.VarPlayer.BIRD_HOUSE_VALLEY_SOUTH;
 
 @Slf4j
 class BirdhouseColoringOverlay extends Overlay {
 
     private final Client client;
-    private final BirdhouseCheckPlugin plugin;
     private final BirdhouseCheckConfig config;
+    private final BirdhouseCheckPlugin plugin;
 
     private static final int MAX_DISTANCE = 2350;
 
     @Inject
     private BirdhouseColoringOverlay(Client client, BirdhouseCheckPlugin plugin, BirdhouseCheckConfig config, ModelOutlineRenderer modelOutlineRenderer)
     {
-
         this.plugin = plugin;
         this.client = client;
         this.config = config;
         setPosition(OverlayPosition.DYNAMIC);
         setPriority(OverlayPriority.LOW);
         setLayer(OverlayLayer.ABOVE_SCENE);
-
     }
-    @Override
-    public Dimension render(Graphics2D graphics) {
 
+    @Override
+    public Dimension render(Graphics2D graphics)
+    {
+        GameObject meadowNorth = plugin.getMeadowNorth();
+        GameObject meadowSouth = plugin.getMeadowSouth();
+        GameObject valleyNorth = plugin.getValleyNorth();
+        GameObject valleySouth = plugin.getValleySouth();
+
+        if (!Objects.isNull(meadowNorth))
+        {
+            int meadowNorthState = client.getVar(BIRD_HOUSE_MEADOW_NORTH);
+            birdhouseStateRenderer(meadowNorth, meadowNorthState, graphics);
+        }
+
+        if (!Objects.isNull(meadowSouth))
+        {
+            int meadowSouthState = client.getVar(BIRD_HOUSE_MEADOW_SOUTH);
+            birdhouseStateRenderer(meadowSouth, meadowSouthState, graphics);
+        }
+
+        if (!Objects.isNull(valleyNorth))
+        {
+            int valleyNorthState = client.getVar(BIRD_HOUSE_VALLEY_NORTH);
+            birdhouseStateRenderer(valleyNorth, valleyNorthState, graphics);
+        }
+
+        if (!Objects.isNull(valleySouth))
+        {
+            int valleySouthState = client.getVar(BIRD_HOUSE_VALLEY_SOUTH);
+            birdhouseStateRenderer(valleySouth, valleySouthState, graphics);
+        }
+
+        return null;
+    }
+
+    private void birdhouseStateRenderer(GameObject birdhouse, int state, Graphics2D graphics)
+    {
         Color unbuiltColor = config.getUnbuiltColor();
         Color unseededColor = config.getUnseededColor();
         Color seededColor = config.getSeededColor();
 
-        if (!Objects.isNull(plugin.getMeadowNorth())) {
-            int state = client.getVar(BIRD_HOUSE_MEADOW_NORTH);
-            if (state == 0) {
-                renderObject(plugin.getMeadowNorth(), graphics, unbuiltColor);
-                log.debug("meadowNORTH=RED");
-            } else if (state % 3 == 0) {
-                renderObject(plugin.getMeadowNorth(), graphics, seededColor);
-                log.debug("meadowNORTH=GREEN");
-            } else {
-                renderObject(plugin.getMeadowNorth(), graphics, unseededColor);
-                log.debug("meadowNORTH=YELLOW");
-            }
+        if (state == 0)
+        {
+            renderObject(birdhouse, graphics, unbuiltColor);
+            log.debug(birdhouse + "rendered RED");
         }
-
-        if (!Objects.isNull(plugin.getMeadowSouth())) {
-            int state2 = client.getVar(BIRD_HOUSE_MEADOW_SOUTH);
-            if (state2 == 0) {
-                renderObject(plugin.getMeadowSouth(), graphics, unbuiltColor);
-                log.debug("meadowSOUTH=RED");
-            } else if (state2 % 3 == 0) {
-                renderObject(plugin.getMeadowSouth(), graphics, seededColor);
-                log.debug("meadowSOUTH=GREEN");
-            } else {
-                renderObject(plugin.getMeadowSouth(), graphics, unseededColor);
-                log.debug("meadowSOUTH=YELLOW");
-            }
+        else if (state % 3 == 0)
+        {
+            renderObject(birdhouse, graphics, seededColor);
+            log.debug(birdhouse + "rendered GREEN");
         }
-
-
-        if (!Objects.isNull(plugin.getValleyNorth())) {
-            int state3 = client.getVar(BIRD_HOUSE_VALLEY_NORTH);
-            if (state3 == 0) {
-                renderObject(plugin.getValleyNorth(), graphics, unbuiltColor);
-                log.debug("valleyNORTH=RED");
-            } else if (state3 % 3 == 0) {
-                renderObject(plugin.getValleyNorth(), graphics, seededColor);
-                log.debug("valleyNORTH=GREEN");
-            }else {
-                renderObject(plugin.getValleyNorth(), graphics, unseededColor);
-                log.debug("valleyNORTH=YELLOW");
-            }
+        else
+        {
+            renderObject(birdhouse, graphics, unseededColor);
+            log.debug(birdhouse + "rendered YELLOW");
         }
-
-        if (!Objects.isNull(plugin.getValleySouth())) {
-            int state4 = client.getVar(BIRD_HOUSE_VALLEY_SOUTH);
-            if (state4 == 0) {
-                renderObject(plugin.getValleySouth(), graphics, unbuiltColor);
-                log.debug("valleySOUTH=RED");
-            } else if (state4 % 3 == 0) {
-                renderObject(plugin.getValleySouth(), graphics, seededColor);
-                log.debug("valleySOUTH=GREEN");
-            } else {
-                renderObject(plugin.getValleySouth(), graphics, unseededColor);
-                log.debug("valleySOUTH=YELLOW");
-            }
-        }
-
-        return null;
-
     }
 
-    private void renderObject(GameObject object, Graphics2D graphics, Color color) {
+    private void renderObject(GameObject object, Graphics2D graphics, Color color)
+    {
         LocalPoint localLocation = client.getLocalPlayer().getLocalLocation();
-        Point mousePosition = client.getMouseCanvasPosition();
+        if (Objects.isNull(localLocation))
+        {
+            return;
+        }
 
+        Point mousePosition = client.getMouseCanvasPosition();
         LocalPoint location = object.getLocalLocation();
 
-        if (localLocation.distanceTo(location) <= MAX_DISTANCE) {
+        if (localLocation.distanceTo(location) <= MAX_DISTANCE)
+        {
             Shape objectClickbox = object.getClickbox();
-            if (objectClickbox != null) {
-
-                if (objectClickbox.contains(mousePosition.getX(), mousePosition.getY())) {
+            if (objectClickbox != null)
+            {
+                if (objectClickbox.contains(mousePosition.getX(), mousePosition.getY()))
+                {
                     graphics.setColor(color.darker());
                     log.debug("Setting Clickbox - Darker");
                 }
-                else {
+                else
+                {
                     graphics.setColor(color);
                     log.debug("Setting Clickbox - Normal");
                 }
